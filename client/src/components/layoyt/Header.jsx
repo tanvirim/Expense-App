dd292b39d80605e164989117a1aa7bbf965d5778
Tanvir Imam Mitul <blackpanther141532@gmail.com>
2023-08-12 18:50:31 +0600

    added logout and username functionality  in Header which includes navbar

--- client/src/components/layoyt/Header.jsx
+++ client/src/components/layoyt/Header.jsx
@@ -1,33 +1,61 @@
-import { Link } from "react-router-dom"
+
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Header = () => {
+
+  //Hooks
+  const navigate = useNavigate()
+  const [loginUser, setLoginuser] = useState({});
+  
+  useEffect(() => {
+    const data = localStorage.getItem("data");
+    if (data) {
+      const loginUser = JSON.parse(data);
+      setLoginuser(loginUser);
+      
+    }
+  }, []);
+
+//Hooks
+
+//other constants
+  const logoutHandler = ()=> {
+    localStorage.removeItem("data")
+    navigate("/login")
+  }
+
+  //other constants
   return (
     <>
-
-<nav className="navbar navbar-expand-lg bg-body-tertiary">
-
-  <div className="container-fluid">
-
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-        <Link className="navbar-brand" to="/">Expense Management</Link>
-        <ul className="navbar-nav mb-2 mb-lg-0 ms-auto">
-            <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to="/">
-                User
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <Link className="navbar-brand" to="/">
+              Expense Management
             </Link>
-            </li>
-        </ul>
+            <ul className="navbar-nav mb-2 mb-lg-0 ms-auto p-1 d-flex align-items-center">
+              <li className="nav-item me-3">{loginUser && loginUser.name}</li>
+              <li className="nav-item">
+                <button className="btn btn-primary" onClick={logoutHandler}>Logout</button>
+              </li>
+            </ul>
+          </div>
         </div>
-
-  </div>
-</nav>
-
-      
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
