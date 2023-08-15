347e00bc8eb97a57f725eb63e256959333a61808
Tanvir Imam Mitul <blackpanther141532@gmail.com>
2023-08-15 13:14:04 +0600

    sendfile directory bug fixed 2

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.use('/api/v1/transections', transactionRoute)
 
 //static
 app.use(express.static(path.join(__dirname, './client/dist')))
+
 app.get('*', (req,res)=>{
     res.sendFile(path.join(__dirname, './client/dist/index.html'))
 })
